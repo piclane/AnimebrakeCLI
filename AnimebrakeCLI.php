3ff57289efaf00475889822dea962052f1503dcd
Yohei Hina <piclane@gmail.com>
2015-05-03 00:17:54 +0900

    リファクタリング

--- AnimebrakeCLI.php
+++ AnimebrakeCLI.php
@@ -3,49 +3,58 @@ require_once __DIR__ . '/cygwin.inc.php';
 require_once __DIR__ . '/config.inc.php';
 require_once __DIR__ . '/MediaInfo.class.php';
 
-$path = _cp(@$argv[1]);
+$inputPath = _cp(@$argv[1]);
+$outputPath = 'D:/Encoded';
 $videoEncoder = 'x265';
 $videoQuality = 20;
 $audioBitrate = 160;
 
 // 引数が空の時は入力ソースの入力を求めるプロンプトを表示する
-if(empty($path)) {
-    while(empty($path)) {
+if(empty($inputPath)) {
+    while(empty($inputPath)) {
         echo "input> ";
-        $path = _cp(fgets(STDIN, 4096));
+        $inputPath = _cp(fgets(STDIN, 4096));
     }
 }
 
-if(is_dir($path)) {
-    $paths = glob("{$path}/*.*");
-} else if(is_file($path)) {
-    $paths = array($path);
+// 入力チェック＆展開
+if(is_dir($inputPath)) {
+    $inputPaths = glob("{$inputPath}/*.*");
+} else if(is_file($inputPath)) {
+    $inputPaths = array($inputPath);
 } else {
-    $paths = glob($path);
+    $inputPaths = glob($inputPath);
 }
-if(empty($paths)) {
-    die("{$path} is not found.\n");
+if(empty($inputPaths)) {
+    die("{$inputPath} is not found.\n");
 }
-foreach($paths as $input) {
-    echo "Scanning {$input}...\n";
-    $videoInfo = MediaInfo::scan($input);
-    $pathInfo = pathinfo($input);
-    $outputTemp = "D:/Encoded/{$pathInfo['filename']}.tmp";
-    $output = "D:/Encoded/{$pathInfo['filename']}.mp4";
+
+natcasesort($inputPaths);
+$videos = VideoFile::fromPaths($inputPaths, $outputPath);
+/** @var VideoFile $video */
+foreach($videos as $video) {
+    if($video->skip) {
+        continue;
+    }
+
+    echo "\n";
+    echo str_repeat('*', getTerminalColumns())."\n";
+    echo "*\n";
+    echo "* [".date('Y-m-d H:i:s')."] {$video->input}\n";
+    echo "*\n";
+    echo str_repeat('*', getTerminalColumns())."\n";
+    echo "\n";
+
+    $videoInfo = MediaInfo::scan($video->input);
     $videoWidth = $videoInfo->get('/video/1', 'Width');
     $videoIsInterlaced = $videoInfo->is('/video/1', 'Scan type', 'interlaced');
     $audioNums = $videoInfo->countChildren('/audio');
-    if(file_exists($output)) {
-        echo "{$output} is exist. skip this.\n";
-        continue;
-    }
-    @unlink($outputTemp);
 
     $options = implode(' ', array(
         // Source Options
-        "--input \"{$input}\"",
+        "--input \"{$video->input}\"",
         // Destination Options
-        "--output \"{$outputTemp}\"",
+        "--output \"{$video->outputTemp}\"",
         "--format mp4",
         "--markers",
         // Video Options
@@ -76,9 +85,66 @@ foreach($paths as $input) {
     $exec = "nice -n 10 \"".BIN_HANDBRAKECLI."\" {$options}";
 
     echo "$ {$exec}\n\n";
+    @unlink($video->outputTemp);
     passthru($exec);
-    rename($outputTemp, $output);
+    rename($video->outputTemp, $video->output);
+    echo "\n";
 }
 
+class VideoFile {
+    /** @var string 入力ファイルのパス */
+    public /* final */ $input;
 
+    /** @var string 出力ファイルのパス */
+    public /* final */ $output;
 
+    /** @var string 一時出力ファイルのパス */
+    public /* final */ $outputTemp;
+
+    /** @var boolean スキップする時 true */
+    public /* final */ $skip;
+
+    /**
+     * 複数のパスをまとめて VideoFile に変換します
+     *
+     * @param string[] $inputPaths 入力ファイルのパスの配列
+     * @param string $outputPath 出力ディレクトリのパス
+     * @return VideoFile[] 変換れた VideoFile の配列
+     */
+    public static function fromPaths(array $inputPaths, $outputPath) {
+        $videos = array();
+        foreach($inputPaths as $inputPath) {
+            $videos[] = new VideoFile($inputPath, $outputPath);
+        }
+        return $videos;
+    }
+
+    /**
+     * コンストラクタ
+     *
+     * @param string $inputPath 入力ファイルのパス
+     * @param string $outputPath 出力ディレクトリのパス
+     */
+    function __construct($inputPath, $outputPath) {
+        $pathInfo = pathinfo($inputPath);
+        $this->input = $inputPath;
+        $this->output = "{$outputPath}/{$pathInfo['filename']}.mp4";
+        $this->outputTemp = "{$outputPath}/{$pathInfo['filename']}.tmp";
+        $this->skip = file_exists($this->output);
+    }
+}
+
+/**
+ * 端末の幅を取得します
+ *
+ * @param int $def デフォルト値
+ * @return int 端末の幅
+ */
+function getTerminalColumns($def = 80) {
+    $cols = exec('tput cols', $output, $retval);
+    if($retval === 0) {
+        return intval($cols);
+    } else {
+        return $def;
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# AnimebrakeCLI.php
+# AnimebrakeCLI
 
 動画ファイルを HandbrakeCLI を使って簡単にエンコードします。  
 AnimebrakeCLIは、複数の音声ファイル・動画のデインターレース処理を自動で行う優れものです(^^)
 
 php /path/to/AnimebrakeCLI.php [input]
 
-動作には以下の物が必要です:
+動作には以下の物が必要です(Required):
+
 - PHP5
 - Handbrake
 - MediaInfo
+
+以下の物もあれば嬉しい(Recommended):
+
+- ncurses (tput)
