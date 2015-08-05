056062af6366f4a35ab9aac7098d403ad91ea139
Yohei Hina <yohei_hina@dwango.co.jp>
2015-08-05 14:26:27 +0900

    Windowsパスからcygwinパスへの変換にcygpathを使用するように変更

--- cygwin.inc.php
+++ cygwin.inc.php
@@ -17,12 +17,5 @@ function _cp($path) {
     if(!IS_CYGWIN_ENV) {
         return $path;
     }
-    $path = str_replace('\\', '/', $path);
-    if(preg_match('!^[\'"](.*)[\'"]$!', $path, $matches)) {
-        $path = $matches[1];
-    }
-    if(preg_match('!^/cygdrive/([^/]+)(.*)$!', $path, $matches)) {
-        $path = strtoupper($matches[1]).':'.$matches[2];
-    }
-    return $path;
+    return exec("cygpath -u '{$path}''");
 }
