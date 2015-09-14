fc6c5953737584ec43d635ccf80055df8c693474
Yohei Hina <piclane@gmail.com>
2015-09-15 00:01:51 +0900

    パーミッションを変更

--- AnimebrakeCLI.php
+++ AnimebrakeCLI.php
@@ -32,6 +32,7 @@ if(empty($inputPaths)) {
 }
 
 natcasesort($inputPaths);
+var_dump($inputPaths);
 $videos = VideoFile::fromPaths($inputPaths, $outputPath);
 /** @var VideoFile $video */
 foreach($videos as $video) {
